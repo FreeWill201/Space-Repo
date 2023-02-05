3dd516ed19759d62f0fcc05cf433cfdd10c356fc
Arlindo Pinto <perensen@gmail.com>
2023-02-05 15:36:08 -0500

    added limitation to datepicked to prevent future dates from being picked

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,8 +1,9 @@
 // when user clicks date text input, show date picker
 $( function() {
     $( "#datepicker" ).datepicker({
-         dateFormat: 'yy-mm-dd' 
+         dateFormat: 'yy-mm-dd' ,
+         maxDate: 0
         })
-        .datepicker("setDate", new Date())     
+        .datepicker("setDate", new Date())  
 } );
 
